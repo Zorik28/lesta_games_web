9e09d4e166cc8c503fe3997d431fa13e3e8ac9d8
Zorik Karapetyan <zorikkarapetan@gmail.com>
2024-04-22 00:22:44 +0300

    first step

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
+
+lesta_games = FastAPI()
+
+
+@lesta_games.get('/', response_class=FileResponse)
+async def get_root():
+    return FileResponse("upload.html")
